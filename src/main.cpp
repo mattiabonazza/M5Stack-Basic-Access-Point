8b3bd1c38d1c4e4f7912a4ddf6d6f7b6c19ed372
mbonazza <mattia.bonazza@gmail.com>
2025-01-15 11:53:28 +0100

    Update main.cpp
    Some comment are provoded

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 // DEMO Program to start a soft Access Point Mode, returns true if successfull
+// Include libraries to interact with M5Stack Core
 #include <M5Stack.h>
 #include <WiFi.h>
 
@@ -11,6 +12,7 @@ void setup() {
 }
 
 void loop() {
+  // Provide a SSID and Password for the soft Access Point
   if (WiFi.softAP("M5Stack_AP", "12345678", 1, 0, 4)) {
     M5.Lcd.println("WiFi AP is set up");
     M5.Lcd.println(WiFi.softAPIP());
